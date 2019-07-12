0af246b4025f73cba346a88f899cd318e34080ce
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-07-11 23:02:05 -0300

    finish rgb to yiq function

--- methods/filters.py
+++ methods/filters.py
@@ -1,113 +1,105 @@
 import sys
-import cv2
+import cv2 as cv
 import math
 import numpy as np
+from PIL import Image
 
 
 def clearMatrix(matrix):
-	newMatrix = matrix
-	for i in range(len(matrix)):
-		for x in range(len(matrix[0])):
-			newMatrix[i][x] = 0
-	return newMatrix
+    newMatrix = matrix
+    for i in range(len(matrix)):
+        for x in range(len(matrix[0])):
+            newMatrix[i][x] = 0
+    return newMatrix
 
 
 def showImage(method, img):
-	cv2.startWindowThread()
-	cv2.imshow(method, img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+    # cv.startWindowThread()
+    # cv.imshow(method, img)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+	im = Image.fromarray(img)
+	im.show()
 
 
 def showRGB(path, band):
-	img = cv2.imread(path, 3)
-	blue, green, red = cv2.split(img)
+    img = cv.imread(path, 3)
+    blue, green, red = cv.split(img)
 
-	if band == 'blue':
-		green = clearMatrix(green)
-		red = clearMatrix(red)
-		imgBlue = cv2.merge([blue, green, red])
-		showImage('Blue', imgBlue)
-	elif band == 'green':
-		blue = clearMatrix(blue)
-		red = clearMatrix(red)
-		imgGreen = cv2.merge([blue, green, red])
-		showImage('Green', imgGreen)
-	else:
-		blue = clearMatrix(blue)
-		green = clearMatrix(green)
-		imgRed = cv2.merge([blue, green, red])
-		showImage('Red', imgRed)
+    if band == 'blue':
+        green = clearMatrix(green)
+        red = clearMatrix(red)
+        imgBlue = cv.merge([blue, green, red])
+        showImage('Blue', imgBlue)
+    elif band == 'green':
+        blue = clearMatrix(blue)
+        red = clearMatrix(red)
+        imgGreen = cv.merge([blue, green, red])
+        showImage('Green', imgGreen)
+    else:
+        blue = clearMatrix(blue)
+        green = clearMatrix(green)
+        imgRed = cv.merge([blue, green, red])
+        showImage('Red', imgRed)
 
 
 def convertToYIQ(path):
-	img = cv2.imread(path, 3)
-	height, width, channel = img.shape
-	blue, green, red = cv2.split(img)
+    img = cv.imread(path, 3)
+    row, col, ch = img.shape
+    blue, green, red = cv.split(img)
 
-	y_matrix = np.zeros((height, width), dtype=np.uint8)
-	i_matrix = np.zeros((height, width), dtype=np.uint8)
-	q_matrix = np.zeros((height, width), dtype=np.uint8)
+    y_matrix = np.zeros((row, col))
+    i_matrix = np.zeros((row, col))
+    q_matrix = np.zeros((row, col))
 
-	for i in range(0, height):
-		for j in range(0, width):
-			r = red[i,j]
-			g = green[i,j]
-			b = blue[i,j]
+    for i in range(row):
+        for j in range(col):
+            r = red[i, j]
+            g = green[i, j]
+            b = blue[i, j]
+            # RGB to YIQ
+            y_matrix[i, j] = (0.299 * r) + (0.587 * g) + (0.114 * b)
+            i_matrix[i, j] = (0.596 * r) - (0.274 * g) - (0.322 * b)
+            q_matrix[i, j] = (0.211 * r) - (0.523 * g) + (0.312 * b)
 
-			#RGB to YIQ
-			y_matrix[i,j] = (0.299 * r) + (0.587 * g) + (0.114 * b)
-			i_matrix[i,j] = (0.596 * r) - (0.274 * g) - (0.322 * b)
-			q_matrix[i,j] = (0.211 * r) - (0.523 * g) + (0.312 * b)
+    return y_matrix, i_matrix, q_matrix
 
-	YIQ_image = cv2.merge([q_matrix, i_matrix, y_matrix])
-	# print('RED PURE', red)
-	# print('')
-	# showImage('YIQ Image', YIQ_image)
-	return y_matrix, i_matrix, q_matrix
 
 def convertToRGB(y_matrix, i_matrix, q_matrix):
-	height = y_matrix.shape[0]
-	width = y_matrix.shape[1]
+    row = y_matrix.shape[0]
+    col = y_matrix.shape[1]
 
-	r_matrix = np.zeros((height, width), dtype=np.uint8)
-	g_matrix = np.zeros((height, width), dtype=np.uint8)
-	b_matrix = np.zeros((height, width), dtype=np.uint8)
+    r_matrix = np.zeros((row, col), dtype=np.uint8)
+    g_matrix = np.zeros((row, col), dtype=np.uint8)
+    b_matrix = np.zeros((row, col), dtype=np.uint8)
 
-	for x in range(0, height):
-		for y in range(0, width):
-			r_matrix[x,y] = 0
-			g_matrix[x,y] = 0
-			b_matrix[x,y] = 0
+    for x in range(0, row):
+        for j in range(0, col):
+            y = y_matrix[x, j]
+            i = i_matrix[x, j]
+            q = q_matrix[x, j]
+            # YIQ to RGB
+            r_matrix[x, j] = abs((1.000 * y) + (0.956 * i) + (0.621 * q))
+            g_matrix[x, j] = abs((1.000 * y) - (0.272 * i) - (0.647 * q))
+            b_matrix[x, j] = abs((1.000 * y) - (1.106 * i) + (1.703 * q))
 
-	for x in range(0, height):
-		for j in range(0, width):
-			y = y_matrix[x,j]
-			i = i_matrix[x,j]
-			q = q_matrix[x,j]
+    # rgb_img = cv.merge([b_matrix, g_matrix, r_matrix])
+    rgb_img = cv.merge([r_matrix, g_matrix, b_matrix])
+    showImage('RGB to YIQ to RGB', rgb_img)
 
-			#YIQ to RGB
-			r_matrix[x,j] = abs((1.000 * y) + (0.956 * i) + (0.621 * q))
-			g_matrix[x,j] = abs((1.000 * y) - (0.272 * i) - (0.647 * q))
-			b_matrix[x,j] = abs((1.000 * y) - (1.106 * i) + (1.703 * q))
-	
-	g_matrix = clearMatrix(g_matrix)
-	r_matrix = clearMatrix(r_matrix)
-	rgb_img = cv2.merge([b_matrix, g_matrix, r_matrix])
-	showImage('RGB', rgb_img)
 
 def rbgToYIQ(path):
-	y_matrix, i_matrix, q_matrix = convertToYIQ(path)
-	convertToRGB(y_matrix, i_matrix, q_matrix)
+    y_matrix, i_matrix, q_matrix = convertToYIQ(path)
+    convertToRGB(y_matrix, i_matrix, q_matrix)
 
 
 def thresholding(path, measure):
-	img = cv2.imread(path, 0)
-	newImg = img
-	for i in range(0, img.shape[0]):
-		for j in range(0, img.shape[1]):
-			if img[i,j] < 50:
-				newImg[i,j] = 0
-			else:
-				newImg[i,j] = 255
-	showImage('Thresholding', newImg)
+    img = cv.imread(path, 0)
+    newImg = img
+    for i in range(0, img.shape[0]):
+        for j in range(0, img.shape[1]):
+            if img[i, j] < 50:
+                newImg[i, j] = 0
+            else:
+                newImg[i, j] = 255
+    showImage('Thresholding', newImg)
