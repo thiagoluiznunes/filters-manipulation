ea78d883fea93a578becaf1dcf0addf08f7e78a8
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-07-17 18:13:55 -0300

    fix createExtendedMatrixes function

--- methods/helper.py
+++ methods/helper.py
@@ -4,43 +4,42 @@ import cv2 as cv
 
 
 def clearMatrix(matrix):
-    newMatrix = matrix
-    for i in range(len(matrix)):
-        for x in range(len(matrix[0])):
-            newMatrix[i][x] = 0
-    return newMatrix
+	newMatrix = matrix
+	for i in range(len(matrix)):
+		for x in range(len(matrix[0])):
+			newMatrix[i][x] = 0
+	return newMatrix
 
 
 def showImage(method, img):
-    im = Image.fromarray(img)
-    im.show()
+	im = Image.fromarray(img)
+	im.show()
 
 
 def createRGBMatrixes(row, col):
-    r_matrix = np.zeros((row, col), dtype=np.uint8)
-    g_matrix = np.zeros((row, col), dtype=np.uint8)
-    b_matrix = np.zeros((row, col), dtype=np.uint8)
-    return r_matrix, g_matrix, b_matrix
+	r_matrix = np.zeros((row, col), dtype=np.uint8)
+	g_matrix = np.zeros((row, col), dtype=np.uint8)
+	b_matrix = np.zeros((row, col), dtype=np.uint8)
+	return r_matrix, g_matrix, b_matrix
 
 
 def createYIQMatrixes(row, col):
-    y_matrix = np.zeros((row, col))
-    i_matrix = np.zeros((row, col))
-    q_matrix = np.zeros((row, col))
-    return y_matrix, i_matrix, q_matrix
+	y_matrix = np.zeros((row, col))
+	i_matrix = np.zeros((row, col))
+	q_matrix = np.zeros((row, col))
+	return y_matrix, i_matrix, q_matrix
 
 
 def createExtendedMatrixes(img, inc):
-    row, col, ch = img.shape
+	row, col, ch = img.shape
 
-    r_extended = np.zeros((row + inc, col + inc), dtype=np.uint8)
-    g_extended = np.zeros((row + inc, col + inc), dtype=np.uint8)
-    b_extended = np.zeros((row + inc, col + inc), dtype=np.uint8)
+	r_extended = np.zeros((row + inc * 2, col + inc * 2), dtype=np.uint8)
+	g_extended = np.zeros((row + inc * 2, col + inc * 2), dtype=np.uint8)
+	b_extended = np.zeros((row + inc * 2, col + inc * 2), dtype=np.uint8)
 
-
-    for i in range(row):
-        for j in range(col):
-            b_extended[i + inc, j + inc] = img[i, j, 0]
-            g_extended[i + inc, j + inc] = img[i, j, 1]
-            r_extended[i + inc, j + inc] = img[i, j, 2]
-    return r_extended, g_extended, b_extended
+	for i in range(row):
+		for j in range(col):
+			b_extended[i + inc, j + inc] = img[i, j, 0]
+			g_extended[i + inc, j + inc] = img[i, j, 1]
+			r_extended[i + inc, j + inc] = img[i, j, 2]
+	return r_extended, g_extended, b_extended
